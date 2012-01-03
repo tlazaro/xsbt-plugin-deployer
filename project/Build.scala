2866ce7300d4ab84d7c55b55cc9a8706d1ec3ae9
Tomás Lázaro <tomas@belfrygames.com>
2012-01-03 16:42:54 -0200

    Figuring out credentials

--- project/Build.scala
+++ project/Build.scala
@@ -1,14 +1,15 @@
 import sbt._, Keys._, Path._
 
 object ProjectDefinition extends Build {
+  credentials += Credentials(file("/private/belfry/.credentials/.credentials"))
+
   lazy val root = Project("xsbt-plugin-deployer", file(".")).settings(extraSettings:_*)
   lazy val launcher = Project("launcher", file("./launcher")) settings (
     publishTo <<= (version) { version: String =>
       val cloudbees = "https://repository-belfry.forge.cloudbees.com/private/"
       if (version.trim.endsWith("SNAPSHOT")) Some("snapshots" at cloudbees + "snapshots/") 
       else                                   Some("releases"  at cloudbees + "releases/")
-    },
-    credentials += Credentials(file("/private/belfry/.credentials/.credentials"))
+    }
   )
 
   def extraSettings = Seq(
